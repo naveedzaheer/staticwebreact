38df7767be1aab7e8230e0097d0efa28f4c70b10
Naveed Zaheer <nzaheer@microsoft.com>
2021-08-09 17:12:00 -0500

    Latestupdates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      fetch(`/api/SayHello`)
+            .then(res => res.json())
+            .then((txtData) => {
+                console.log('Function Data:' + JSON.stringify(txtData));
+                setData(txtData.message);
+              })
+            .catch('Exception' + console.log)
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
